6e5b48dec1dc1ab3671c18cd2cb9941bbf170bca
aymeric_dgx <128166467+Aymeric-dgx@users.noreply.github.com>
2025-10-22 18:59:08 +0200

    Create main.c

---
+++ 0_test/syntaxe_header/main.c
@@ -0,0 +1,9 @@
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
+#include <stdio.h>
+#include "header.h"
+
+int main(int argc, char* argv[]) {
+    // Appelle de la fonction depuis le hedear.h
+    init_playerr();
+}
